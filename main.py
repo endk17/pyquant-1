bfcd67538f711e157cffe9591e5bc9fd894bd4fb
Enda Kelly <kelly.enda@gmail.com>
2022-09-15 22:13:01 +0100

    added: non-etf wiht high vol

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-# region imports
 from AlgorithmImports import *
-# endregion
 
-class JumpingRedOrangeAntelope(QCAlgorithm):
+class PowerEarningsGap(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2020, 2, 17)  # Set Start Date
-        self.SetCash(100000)  # Set Strategy Cash
-        self.AddEquity("SPY", Resolution.Minute)
-        self.AddEquity("BND", Resolution.Minute)
-        self.AddEquity("AAPL", Resolution.Minute)
+        self.SetStartDate(2020, 7, 20)
+        self.SetEndDate(2022, 9, 15)
+        self.SetCash(10000)
 
-    def OnData(self, data: Slice):
-        if not self.Portfolio.Invested:
-            self.SetHoldings("SPY", 0.33)
-            self.SetHoldings("BND", 0.33)
-            self.SetHoldings("AAPL", 0.33)
+        # add SPY so that we can use it in the schedule rule below
+        self.SPY = self.AddEquity('SPY', Resolution.Minute).Symbol
+
+        # build a universe using the CoarseFilter and FineFilter functions defined below
+        self.AddUniverse(self.CoarseFilter)
+
+
+    def CoarseFilter(self, universe):
+        # filter universe, ensure DollarVolume is above a certain threshold
+        # also filter by assets that have fundamental data
+        universe = [asset for asset in universe if asset.DollarVolume > 1000000 and asset.Price > 10 and asset.HasFundamentalData]
+        
+        # sort universe by highest dollar volume
+        sortedByDollarVolume = sorted(universe, key=lambda asset: asset.DollarVolume, reverse=True)
+        
+        # only select the first 500
+        topSortedByDollarVolume = sortedByDollarVolume[:500]
+
+        # we must return a list of the symbol objects only
+        symbolObjects = [asset.Symbol for asset in topSortedByDollarVolume]
+
+        # this line is not necessary, but we will use it for debugging to see a list of ticker symbols
+        tickerSymbolValuesOnly = [symbol.Value for symbol in symbolObjects]
+
+        return symbolObjects
